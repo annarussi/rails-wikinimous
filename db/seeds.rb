f3f91179062113800529666f10bf6f5f84098d06
Anna Russi <annarussi@gmail.com>
2023-05-23 16:49:35 +0100

    backend wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+# TODO: Write a seed to insert 100 posts in the database
+puts 'Creating 10 fake posts...'
+
+10.times do
+  Article.create(title: Faker::Book.title, content: Faker::Books::Dune.quote)
+end
